b00ee5fd6b2e42b1ae1382428d0ced92b0987309
mca-programmer <freelancermca52@gmail.com>
2025-12-01 13:51:55 +0600

    added mongodb

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ app.use(cors());
 app.use(express.json())
 
 
+//vIguEMDXiP3GEI4G
+//assignment10
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mca-programmer.xd7qlcv.mongodb.net/?appName=mca-programmer`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+   // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
 }
 run().catch(console.dir);
 app.listen(port, () => {
